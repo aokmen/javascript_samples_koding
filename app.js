e9846104838d51c0cd8c955f36f8b0b509675c35
aokmen <abdullah.okmen.11@gmail.com>
2023-05-18 21:14:05 +0200

    new

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
 //? dfg
 // todo 
 //! importing 
-console.log("This is a console line")
-console.error("This is error")
-console.warn("This a warn")
-console.clear();
-console.table([{name:"Tom",age:30},{name:"Bill",age:38}])
-alert("This a alert")
-document.write("Show in browser")
-const ad = prompt("your name:")
-console.log(ad);
+// console.log("This is a console line")
+// console.error("This is error")
+// console.warn("This a warn")
+// console.clear();
+// console.table([{name:"Tom",age:30},{name:"Bill",age:38}])
+// alert("This a alert")
+// document.write("Show in browser")
+// const ad = prompt("your name:")
+// console.log(ad);
 
-console.clear();
+// console.clear();
 
-const varName ="new value";
-console.log(varName,"is a language");
+// const varName ="new value";
+// console.log(varName,"is a language");
 
-const number1=45;
-console.log(number1+54);
+// const number1=45;
+// console.log(number1+54);
 
-var text="This is a value key";
-console.log(text);
+// var text="This is a value key";
+// console.log(text);
 
 // todo const = constant (sabit/degismeyen)
-//? Global Scope: Program içerisinde fonksiyonların dışında tanımlanan ve her yerden erişilebilen scope tipidir.
-//? Kodu yamaya bağladığımızda global scope yazarız.SAdece tek global scope var.
+// //? Global Scope: Program içerisinde fonksiyonların dışında tanımlanan ve her yerden erişilebilen scope tipidir.
+// //? Kodu yamaya bağladığımızda global scope yazarız.SAdece tek global scope var.
 
-//? Local Scope : Bir block içi (fonksiyon, if bloğu  gibi alanlar)
+// //? Local Scope : Bir block içi (fonksiyon, if bloğu  gibi alanlar)
 
-const number=200;
-console.log(number);
+// const number=200;
+// console.log(number);
 
-const pi=3.14;
-console.log(pi);
+// const pi=3.14;
+// console.log(pi);
 
-const global ="this is const global";
-{
-    const local ="this is const local";
-    console.log(global);
-    console.log(local);
-}
+// const global ="this is const global";
+// {
+//     const local ="this is const local";
+//     console.log(global);
+//     console.log(local);
+// }
 
-let people=300;
-people=300+5;
-console.log(people);
-console.clear();
-let global2 ="this is let global";
-var local3 ="this is var local000";
-{
-    let local2 ="this is let local";
-    var local3 ="this is var local";
-    console.log(global2);
-    console.log(local2);
+// let people=300;
+// people=300+5;
+// console.log(people);
+// console.clear();
+// let global2 ="this is let global";
+// var local3 ="this is var local000";
+// {
+//     let local2 ="this is let local";
+//     var local3 ="this is var local";
+//     console.log(global2);
+//     console.log(local2);
     
-}
-console.log(local3);
+// }
+// console.log(local3);
 
-{
-    const name1=41;
-name1=43;
+// {
+//     const name1=41;
+// name1=43;
 
-console.log(name1);
+// console.log(name1);
     
-}
+// }
 console.clear();
 let b=2n;
 let a="10";
--- cal.js
+++ cal.js
@@ -1,48 +1,48 @@
 // console.log("in cal")
 console.clear();
-function sevenboom(arr){
-    let b = "";
-     arr.forEach(n => { 
+// function sevenboom(arr){
+//     let b = "";
+//      arr.forEach(n => { 
       
-      if (n.search("7") == true) { 
-        b = "Boom!"};
+//       if (n.search("7") == true) { 
+//         b = "Boom!"};
         
-      }
-    return b;
-  }
+//       }
+//     return b;
+//   }
    
    
   
-  const sevenboom = (arr) => {
-    let b = "";
-    arr.forEach(n => { 
+  // const sevenboom = (arr) => {
+  //   let b = "";
+  //   arr.forEach(n => { 
       
-      if (n.search("7") == true) { 
-        b = "Boom!"};
+  //     if (n.search("7") == true) { 
+  //       b = "Boom!"};
         
-      }
-      return b;
-  };
+  //     }
+  //     return b;
+  // };
   
-  numbers.forEach(function(number) {
-      if (n.search("7") == true) {
-          numbers.shift() // 3 will be deleted from array
-      }
-  })
+  // numbers.forEach(function(number) {
+  //     if (n.search("7") == true) {
+  //         numbers.shift() // 3 will be deleted from array
+  //     }
+  // })
   
-  const array1 = [5, 12, 8, 130, 44];
+//   const array1 = [5, 12, 8, 130, 44];
 
-  const found = array1.find(element => element == element.find("3"));
+//   const found = array1.find(element => element == element.find("3"));
   
-  console.log(found);
-  
-  
+//   console.log(found);
   
   
-  console.log(sevenboom([1, 2, 3, 4, 5, 6, 7]));
-  console.log(sevenboom([8, 6, 33, 100]));
-  console.log(sevenboom([2, 55, 60, 97, 86]));
-  console.log(sevenboom([2, 57, 60, 97, 86]));
+  
+  
+//   console.log(sevenboom([1, 2, 3, 4, 5, 6, 7]));
+//   console.log(sevenboom([8, 6, 33, 100]));
+//   console.log(sevenboom([2, 55, 60, 97, 86]));
+//   console.log(sevenboom([2, 57, 60, 97, 86]));
 let a=prompt("sayiyi gir");
 
 if (a>79){
